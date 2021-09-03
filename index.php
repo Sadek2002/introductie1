f140dcbe3930079e9d6f98fd93ce2fb92c6a206f
Sadek <sharkalmousawi@live.com>
2021-09-03 11:41:26 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,70 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<link rel="stylesheet" href="style.css">
+<body>
+
+<div class="container">
+    <p class="month">September</p>
+    <p class="task">MySQL classroom Toets 1 en PHP maken en bezig zijn met stage zoeken.</p>
+</div>
+<div></div><br>
+<div class="container">
+    <p class="month">Oktober</p>
+    <p class="task">MySQL classroom Toets 2 en Database classroom maken.</p>
+</div>
+<div class="container">
+    <p class="month">November</p>
+    <p class="task">Beginnen aan 1 van de 3 keuzedelen en afkrijgen als ik geen stage heb of aan stage beginnen als ik er een heb.</p>
+</div>
+<div class="container">
+    <p class="month">December</p>
+    <p class="task">Begginen aan 2de keuzedeel en het afkrijgen als ik geen stage heb of verder aan stage werken.</p>
+</div>
+
+<div class="container">
+    <p class="month">Januari</p>
+    <p class="task">Beginnen aan de 3de keuzedeel en het afkrijgen als het mogelijk is of 2de keuzedeel afmaken als ik een stage heb verder aan stage werken</p>
+</div>
+
+<div class="container">
+    <p class="month">Februari</p>
+    <p class="task">Werken aan basis vakken of verden werken aan stage</p>
+</div>
+
+<div class="container">
+    <p class="month">Maart</p>
+    <p class="task">Basis vakken examens doen of verder werken aan stage</p>
+</div>
+
+<div class="container">
+    <p class="month">April</p>
+    <p class="task">Verder werken aan stage stage als het nog niet af is</p>
+</div>
+
+<div class="container">
+    <p class="month">Mei</p>
+    <p class="task">Alle examens afronden</p>
+</div>
+
+<div class="container">
+    <p class="month">Juni</p>
+    <p class="task">Schooljaar afgerond</p>
+</div>
+
+<div class="container">
+    <p class="month">Juli</p>
+    <p class="task">Schooljaar afgerond</p>
+</div>
+
+<div class="text"><p class="hoofd">Reflectie Verslag</p>
+<p class="verslag">Ik doe de opleiding software developer al 2 jaar en ben begonnen met de 3de schooljaar. Ik heb matig gepresteerd kan beter ,maar vanwege de corona lockdown had ik geen motivatie meer. Ik ben tevreden over mijn prestatie over de eerste stage en had ook freecodecamp gedaan op een snelle tempo! Ik loop wat achter op schema ,maar ik heb nu wel de motivatie om hard te werken. Ik vond het wel goed hoe ik aan de stage bit project werkte. Mijn verbeter punten is dat ik sneler en meer moet concentreren op mijn werk ,zodat ik mijn diploma kan halen dit jaar. Ik zou meer kennis over html en php willen leren maar het html en style gaat het best goed! Hoe ik het zie op stage het gaat best wel met de kennis van bit project denk ik wel dat het wel goed komt.</p></div>
+
+</body>
+</html>
